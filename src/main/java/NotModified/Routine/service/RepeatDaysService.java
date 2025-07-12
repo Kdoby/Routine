9193f5a9cf5c1dbff1d91dfd01737bd3785eb702
doyeon <doyun20445@gmail.com>
2025-07-12 15:57:17 +0900

    feat: 루틴별 반복 요일 등록 구현

---
+++ src/main/java/NotModified/Routine/service/RepeatDaysService.java
@@ -0,0 +1,27 @@
+package NotModified.Routine.service;
+
+import NotModified.Routine.domain.RepeatDays;
+import NotModified.Routine.dto.routine.request.RoutineCreateRequest;
+import NotModified.Routine.repository.interfaces.RepeatDaysRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class RepeatDaysService {
+    private final RepeatDaysRepository repeatDaysRepository;
+
+    public void registerRepeatDays(Long routineId, List<Integer> days) {
+        for(Integer day : days) {
+            repeatDaysRepository.save(RepeatDays.builder()
+                    .routineId(routineId)
+                    .weekOfDay(day)
+                    .build());
+        }
+    }
+
+}
